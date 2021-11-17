23197905f4afde649065578735319414b03bb350
xmichelle26x <xmichelle26x@gmail.com>
2021-11-16 19:04:17 -0500

    Points Function

--- functions.py
+++ functions.py
@@ -3,17 +3,36 @@ points = 0
 playerOne = 0
 playerTwo = 0
 
-playerOne = {
-    "puntaje": 0,
-    "nombre": "playerOne"
-}
+point = [0,15,30,40]
 
-playerTwo= {
-    "puntaje": 0,
-    "nombre": "playerTwo"
-}
+# playerOne = {
+#     "zero": 0,
+#     "fifteen":15,
+#     "thirty":30,
+#     "forty":40,
+#     "name": "playerOne"
+# }
 
-def puntuacion():
-    playerOne.values()[0] = 15
+# playerTwo= {
+#     "zero": 0,
+#     "fifteen":15,
+#     "thirty":30,
+#     "forty":40,
+#     "name": "playerTwo"
+# } 
 
-print puntuacion()
+def sumpoints():
+    if playerOne == 0 and playerTwo == 0:
+        print("PlayerOne {0} PlayerTwo {1}".format(playerOne, playerTwo)) 
+    if playerOne == 30 and playerTwo == 15:
+        print("PlayerOne {0} PlayerTwo {1}".format(playerOne, playerTwo)) 
+    if playerOne == 40 and playerTwo == 40:
+        print("Deuce") 
+    if playerOne == 41 and playerTwo == 40:
+        print("Advantage PlayerOne")
+    if playerOne == 40 and playerTwo == 41:
+        print("Advantage PlayerTwo")
+    if playerOne == 40 and playerTwo == 42:
+        print("PlayerTwo wins")
+    
+(sumpoints())
